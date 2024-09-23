af3a2299b11a0f316ed68b769327b9f4daf24965
Paula-Cabral <131957774+Paula-Cabral@users.noreply.github.com>
2024-09-23 19:19:39 +0100

    Criar classe aritmética e adicionar

---
+++ Aritmética.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    //Implementa operações aritméticas
+    class Aritmética
+    {
+        public int Somar(int x, int y)
+        {
+
+            return x + y;
+        }
+    }
+}
