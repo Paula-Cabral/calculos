8c3ea1bf13c9788ef94b1ea3d4f3bb38996deceb
Paula-Cabral <131957774+Paula-Cabral@users.noreply.github.com>
2024-09-23 19:48:43 +0100

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -14,6 +14,11 @@ namespace Calculos
             Console.WriteLine($"4+2={Aritmética.Somar(4,2) }");
             Console.WriteLine($"4-2={Aritmética.Subtrair(4, 2) }");
 
+            double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Farheneheit = {t2}º Celsius");
             Console.ReadKey();
         }
     }
